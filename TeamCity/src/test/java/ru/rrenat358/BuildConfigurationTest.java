f3827509e252ea17d6918c771be9e56acf3fc525
rrenat358 <rrenat358@gmail.com>
2023-11-30 20:34:15 +0300

    2034

--- TeamCity/src/test/java/ru/rrenat358/BuildConfigurationTest.java
+++ TeamCity/src/test/java/ru/rrenat358/BuildConfigurationTest.java
@@ -1,10 +1,22 @@
 package ru.rrenat358;
 
 
+import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
 
 public class BuildConfigurationTest extends BaseApiTest {
 
 
+    @Test
+    public void buildConfiguration() {
+
+        String token = RestAssured.get("http://user01:1111@localhost:8111/authenticationTest.html?csrf")
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().asString();
+        System.out.println(token);
+
+    }
 
 
 }
