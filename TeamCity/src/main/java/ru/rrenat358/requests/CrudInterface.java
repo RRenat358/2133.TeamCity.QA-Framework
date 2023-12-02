fa42913d72106ac99cf42fc9f738447b86c74ec3
rrenat358 <rrenat358@gmail.com>
2023-12-02 19:16:29 +0300

    1916 create

---
+++ TeamCity/src/main/java/ru/rrenat358/requests/CrudInterface.java
@@ -0,0 +1,14 @@
+package ru.rrenat358.requests;
+
+public interface CrudInterface {
+
+    public Object create();
+
+    public Object get(String string);
+
+    public Object update(String string);
+
+    public Object delete(String string);
+
+
+}
