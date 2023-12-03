37e3be2e6c7546d1237f2334f5543f760b636feb
rrenat358 <rrenat358@gmail.com>
2023-12-03 20:50:53 +0300

    2050

--- TeamCity/src/test/java/ru/rrenat358/BaseApiTest.java
+++ TeamCity/src/test/java/ru/rrenat358/BaseApiTest.java
@@ -2,23 +2,25 @@ package ru.rrenat358;
 
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import ru.rrenat358.TestData.DataEntity;
 import ru.rrenat358.TestData.DataGenerator;
 
 public class BaseApiTest extends BaseTest {
 
-    public static DataGenerator dataGenerator;
-    public static DataEntity dataEntity;
+    public  DataGenerator dataGenerator;
+    public  DataEntity dataEntity;
 
-    @BeforeAll
-    public static void setupTest() {
+    @BeforeEach
+    public  void setupTest() {
         dataEntity = dataGenerator.entityDataGenerator();
     }
 
 
-    @AfterAll
-    public static void cleanTestData() {
+    @AfterEach
+    public void cleanTestData() {
         dataEntity.deleteNewProjectDescription();
     }
 
